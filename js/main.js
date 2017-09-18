9128f60a1656055fab7410a61ecba12999dde9f0
Carlos Cessa <carlos@pixative.com>
2017-09-18 18:25:59 -0500

    Register Form

--- js/main.js
+++ js/main.js
@@ -279,56 +279,56 @@ var GobMXMiSalud    = {
         });
     },  
 
-    setupCustomSelect 	: function () {
-	    $('.custom-select').each(function(){
-		    var $this = $(this), numberOfOptions = $(this).children('option').length;
+    setupCustomSelect   : function () {
+        $('.custom-select').each(function(){
+            var $this = $(this), numberOfOptions = $(this).children('option').length;
 
-		    $this.addClass('select-hidden');
-		    $this.wrap('<div class="select"></div>');
-		    $this.after('<div class="select-styled"></div>');
+            $this.addClass('select-hidden');
+            $this.wrap('<div class="select"></div>');
+            $this.after('<div class="select-styled"></div>');
 
-		    var $styledSelect = $this.next('div.select-styled');
-		    $styledSelect.text($this.children('option').eq(0).text());
+            var $styledSelect = $this.next('div.select-styled');
+            $styledSelect.text($this.children('option').eq(0).text());
 
-		    var $list = $('<ul />', {
-		        'class': 'select-options'
-		    }).insertAfter($styledSelect);
+            var $list = $('<ul />', {
+                'class': 'select-options'
+            }).insertAfter($styledSelect);
 
-		    for (var i = 0; i < numberOfOptions; i++) {
-		        $('<li />', {
-		            text  : $this.children( 'option' ).eq( i ).text(),
-		            rel   : $this.children( 'option' ).eq( i ).val(),
+            for (var i = 0; i < numberOfOptions; i++) {
+                $('<li />', {
+                    text  : $this.children( 'option' ).eq( i ).text(),
+                    rel   : $this.children( 'option' ).eq( i ).val(),
                     class : $this.children( 'option' ).eq( i ).attr( 'disabled' ) === 'disabled' ? 'disabled' : '',
-		        }).appendTo($list);
-		    }
+                }).appendTo($list);
+            }
 
-		    var $listItems = $list.children('li');
+            var $listItems = $list.children('li');
 
-		    $styledSelect.click(function(e) {
-		        e.stopPropagation();
-		        $('div.select-styled.active').not(this).each(function(){
-		            $(this).removeClass('active').next('ul.select-options').hide();
-		        });
-		        $(this).toggleClass('active').next('ul.select-options').toggle();
-		    });
+            $styledSelect.click(function(e) {
+                e.stopPropagation();
+                $('div.select-styled.active').not(this).each(function(){
+                    $(this).removeClass('active').next('ul.select-options').hide();
+                });
+                $(this).toggleClass('active').next('ul.select-options').toggle();
+            });
 
-		    $listItems.click(function(e) {
+            $listItems.click(function(e) {
                 e.stopPropagation();
                 
-		        if (this.className !== 'disabled') {
+                if (this.className !== 'disabled') {
                     $styledSelect.text($(this).text()).removeClass('active');
                     $this.val($(this).attr('rel'));
                     $this.trigger('change');
                     $list.hide();
                 }
-		    });
+            });
 
-		    $(document).click(function() {
-		        $styledSelect.removeClass('active');
-		        $list.hide();
-		    });
+            $(document).click(function() {
+                $styledSelect.removeClass('active');
+                $list.hide();
+            });
 
-		});
+        });
     },
 
     setupRegister   : function () {
@@ -409,6 +409,18 @@ var GobMXMiSalud    = {
             }
          }
             return false;
+        });
+
+        mediaEl.change( function () {
+            if ( mediaEl.val() == 'sms' ) {
+                $( '.form-submit' ).removeClass( 'submit-fixed' );
+                phoneEl.fadeIn();
+                nameEl.fadeIn();
+            } else {
+                $( '.form-submit' ).addClass( 'submit-fixed' );
+                phoneEl.fadeOut();
+                nameEl.fadeOut();
+            }
         });
     },
 
