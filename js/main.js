f00b930a2072613277ecd0a45754a5a2fd99c05b
Giovanni Mendoza <mendozagioo@gmail.com>
2018-02-13 15:30:59 -0600

    Se quitó el texto 'próximamente' del select de registro y se vinculó a la cuenta de twitter

--- js/main.js
+++ js/main.js
@@ -470,6 +470,22 @@ var GobMXMiSalud    = {
             var media   = mediaEl.val();
             if ( media == 'facebook' ) {
                 window.open( 'http://m.me/gobmxmisalud', '_blank' );
+            } else if (media == 'Twitter DM') {
+                var captchaResponse = grecaptcha.getResponse();
+                if (captchaResponse == '') {
+                    $('#messageModal .modal-body p').text("No pudimos completar tu registro. Por favor revisa que todos los datos sean correctos.");
+                    $('#messageModal').modal('show');
+                } else {
+                    $('#ttRegister .modal-body p').text('').append('' +
+                        '<span>¡Hola! Para finalizar tu inscripción en Twitter manda un mensaje privado (DM) con la palabra ALTA a <a href="https://twitter.com/misalud_mx" target="_blank">@misalud_mx</a>.</span>' +
+                        '<span>No es necesario que nos sigas para hacerlo.</span>')
+                        ;
+                    $('#ttRegister').modal('show');
+                    $('#ttRegister .btn-prima').on('click', function (e) {
+                        e.stopPropagation();
+                        $('#ttRegister').modal('hide');
+                    });
+                }
             } else if ( media == 'sms' ) {
                 var captchaResponse     = grecaptcha.getResponse();
                 if ( captchaResponse == '' ) {
